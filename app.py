9ad0c23a8acf59558115203493a0270922eacb4c
Harshavarthan05 <harshavarthanbaskaran05@gmail.com>
2026-07-14 21:44:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,8 +87,6 @@ with gr.Blocks(title="YouTube Video Retriever") as demo:
         inputs=question,
         outputs=answer
     )
-
-demo.launch(server_name="0.0.0.0",
-            server_port=7860,
-            show_error=True
-           )
+    
+if __name__ == "__main__":
+    demo.launch(show_error=True)
